8da35e62158e20cc351e5970a71cc4cce2064b9f
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-09-09 14:49:45 -0400

    adds structure for login, logout functions to AuthContext

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,19 +1,22 @@
 import { PropsWithChildren, createContext, useState } from "react";
 type AuthContextType = {
-  token: string;
+  token?: string;
+  login?: Function;
+  logout?: Function;
 };
-const defaultAuthContext: AuthContextType = { token: "" };
+const defaultAuthContext: AuthContextType = {};
 export const AuthContext = createContext(defaultAuthContext);
 
 export const AuthProvider = (props: PropsWithChildren) => {
   // state to hold auth token
   const [token, setToken] = useState("");
   // login function - takes a username and password, and attempts to log in to the backend
-
+  const login = (username: string, password: string) => {};
   // logout function - clears the auth token
+  const logout = () => {};
 
   return (
-    <AuthContext.Provider value={{ token }}>
+    <AuthContext.Provider value={{ token, login, logout }}>
       {props.children}
     </AuthContext.Provider>
   );
