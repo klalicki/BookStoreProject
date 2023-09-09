0d2bc62ddd20f4d903b15ae5767b91862bb318e9
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-09-09 14:29:32 -0400

    sets up routes for app

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,24 +1,21 @@
 import React from "react";
 import logo from "../logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <main>
+        <Routes>
+          <Route path="/bookshelf" element={<h2>Bookshelf</h2>} />
+          <Route path="/" element={<h2>Login</h2>} />
+          <Route path="/book/:bookID" element={<h2>book</h2>} />
+          <Route path="/search" element={<h2>Search</h2>} />
+        </Routes>
+      </main>
+      <footer></footer>
     </div>
   );
 }
