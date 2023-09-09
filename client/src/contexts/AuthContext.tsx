f74b9ecfc964957e889b9cb5e44571410f5ee699
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-09-09 14:40:07 -0400

    adds types and rough structure to AuthContext

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,10 +1,16 @@
 import { PropsWithChildren, createContext, useState } from "react";
-export const AuthContext = createContext();
+type AuthContextType = {
+  token: string;
+};
+const defaultAuthContext: AuthContextType = { token: "" };
+export const AuthContext = createContext(defaultAuthContext);
 
 export const AuthProvider = (props: PropsWithChildren) => {
   // state to hold auth token
   const [token, setToken] = useState("");
-  // login function - takes a
+  // login function - takes a username and password, and attempts to log in to the backend
+
+  // logout function - clears the auth token
 
   return (
     <AuthContext.Provider value={{ token }}>
