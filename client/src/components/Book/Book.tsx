81e089349e83ab27ebe219b642f10d9843b6aae4
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-09-11 20:46:40 -0400

    adds SWR, starts to build Book component

--- client/src/components/Book/Book.tsx
+++ client/src/components/Book/Book.tsx
@@ -1,11 +1,25 @@
+import axios from "axios";
 import { useParams } from "react-router-dom";
+import useSWR from "swr";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
 const Book = () => {
+  const { token } = useContext(AuthContext);
   const params = useParams();
-  console.log(params);
+  const fetchData = async (url: string) => {
+    return axios.get(url, { headers: { Authorization: `Bearer ${token}` } });
+  };
+  const { data, error, isLoading } = useSWR(
+    `/api/book/${params.bookID}`,
+    fetchData
+  );
+  console.log(data);
   return (
     <div>
       <h3>Individual Book component</h3>
       <h2>Book ID: {params.bookID}</h2>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.toString()}</p>}
     </div>
   );
 };
