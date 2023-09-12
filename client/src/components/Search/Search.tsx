5d7a9af476cde4400a17e63fb82ed3a1a71b34c4
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-09-12 10:01:12 -0400

    adds search box to Search component, pulls value from search params

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 const Search = () => {
-  return <h2>Search</h2>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get("q"));
+  let defaultSearch = searchParams.get("q") || "";
+
+  const [searchQuery, setSearchQuery] = useState(defaultSearch);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+    setSearchParams({ q: e.target.value });
+  };
+  return (
+    <div>
+      <h2>Search</h2>
+      <input type="text" value={searchQuery} onChange={handleSearch} />
+    </div>
+  );
 };
 export default Search;
