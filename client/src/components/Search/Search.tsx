37ff11cd3a850cf0e3dddc0bd60fabf25c75c0a9
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-09-13 00:44:41 -0400

    adds code to Search.tsx to query API for list of books

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import axios from "axios";
 
 const Search = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   console.log(searchParams.get("q"));
   let defaultSearch = searchParams.get("q") || "";
-
   const [searchQuery, setSearchQuery] = useState(defaultSearch);
+
+  useEffect(() => {
+    const fetchSearch = async () => {
+      const { data } = await axios.get(`api/book/search/${searchQuery}`);
+      console.log(data);
+    };
+    fetchSearch();
+  }, [searchQuery]);
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
