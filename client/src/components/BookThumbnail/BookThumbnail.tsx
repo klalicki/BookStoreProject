09310a87f8d5585441ac0174384b817b2da81603
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-09-16 15:24:26 -0400

    adds link to book page to search results thumbnail

--- client/src/components/BookThumbnail/BookThumbnail.tsx
+++ client/src/components/BookThumbnail/BookThumbnail.tsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 const BookThumbnail = ({ book }: { book: any }) => {
   return (
     <div>
+      <Link to={`/book/${book.id}`}>Link</Link>
       <h2>{book.title}</h2>
       <p>{JSON.stringify(book)}</p>
     </div>
